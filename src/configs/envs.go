f8d6b92d50e75e801e7d8f4809d6457f04f7b3e9
nfel <nfilsaraee@gmail.com>
2023-06-16 22:26:49 +0330

    ~

--- src/configs/envs.go
+++ src/configs/envs.go
@@ -40,7 +40,7 @@ type config struct {
 	DEV                          bool          `env:"DEV_DEBUG" envDefault:"false"`
 	LimitUsers                   bool          `env:"LIMIT_USERS" envDefault:"false"`
 	BlockScannerURL              url.URL       `env:"BS_URL" envDefault:"http://154.49.243.32:6001"`
-	UserAppUrl                   url.URL       `env:"USER_APP_URL" envDefault:"https://ua.piper.finance"`
+	UserAppUrl                   url.URL       `env:"USER_APP_URL" envDefault:"http://154.49.243.32:4500"`
 	TokenListUrl                 url.URL       `env:"TOKEN_LIST_URL" envDefault:"https://github.com/PiperFinance/CD/blob/main/tokens/outVerified/all_tokens.json?raw=true"`
 	TokensDir                    string        `env:"TOKEN_LIST_DIR" envDefault:"data/all_tokens.json"`
 	ZapLogLevel                  zapcore.Level
